1c578f94dcd5c5893408f89847ada06568a0ceb5
shion0625 <kaito.yodogawa19@kit.edu.kh>
2021-11-02 23:18:38 +0900

    websocketとratchatの実装

--- html/src/Chat.php
+++ html/src/Chat.php
@@ -19,8 +19,7 @@ class Chat implements MessageComponentInterface {
     public function onMessage(ConnectionInterface $from, $msg) {
         $numRecv = count($this->clients) - 1;
         echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
+        , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
         foreach ($this->clients as $client) {
             if ($from !== $client) {
                 // The sender is not the receiver, send to each client connected
--- html/views/home.php
+++ html/views/home.php
@@ -1,4 +1,7 @@
 <?php
+
+use Symfony\Component\HttpFoundation\Cookie;
+
 require_once __DIR__ . '/../function.php';
 require  __DIR__ . '/../db_function.php';
 
@@ -13,11 +16,12 @@ if(isset($_SESSION['userID']) && $_SESSION['time'] + 3600 > time()) {
 
 // if($_SERVER['REQUEST_METHOD'] != 'POST') {
 //   $message="無効なメソッドでの送信です。";
-// }
+//
+//データベースに投稿ないようを保存
 if(!empty($_POST) && isset($_POST['send'])) {
   $user_id = $_SESSION['userID'];
   $date_time = date("Y-m-d H:i:s");
-  $tweet_content = fun_h($_POST['tweet-content']);
+  $tweet_content = fun_h($_POST['tweet-input']);
   $flag = db_insert_tweet($dbh, $user_id, $date_time, $tweet_content);
   if($flag) {
     $message_alert = "ツイートに成功しました。";
@@ -32,10 +36,12 @@ if(!empty($_POST) && isset($_POST['send'])) {
   }
 }
 
-$posts = db_get_tweets($dbh);
+$db_posts = db_get_tweets($dbh);
+
 ?>
 
 <script>
+  'use strict';
   $(()=> {
     const popup = $('#js-popup');
     if(!popup) return;
@@ -49,56 +55,101 @@ $posts = db_get_tweets($dbh);
       popup.toggleClass('is-show');
     });
   });
-
-const conn = "";
-function open() {
-  const conn = new WebSocket('wss://localhost:8081');
-  console.log('connect');
-  conn.onopen = function(e) {
+//ウェブソケットを使用して送信されたデータをサーバサイドに送信
+let conn = "";
+function open(){
+  conn = new WebSocket('ws://localhost:8081');
+  // if(conn && conn.readyState === 1) return false;
+  conn.onopen = function(event) {
+    console.log('connect');
     console.log("Connection established!");
   };
-  conn.onerror = function(e) {
-    alert("エラーが発生しました。");
-  }
+
+  conn.onerror = function(event) {
+    alert("エラーが発生しました");
+  };
+
+  conn.onmessage = function(event) {
+    console.log(event.data);
+    const dataArray = event.data.split(">");
+    const postObj = document.getElementById("js-posts");
+    let divObj = document.createElement("DIV");
+    divObj.className = "post";
+    postObj.prepend(divObj);
+    for(let i in dataArray) {
+      let pObj = document.createElement("P");
+      divObj.appendChild(pObj);
+      let data = document.createTextNode(dataArray[i].trim());
+      console.log(data);
+      if(i == 0) {
+        let imgObj = document.createElement("img");
+        let spanObj = document.createElement("span");
+        pObj.className="post-user-detail";
+        imgObj.className="user-post-img";
+        spanObj.className="tweet-username";
+        pObj.appendChild(imgObj);
+        spanObj.appendChild(data);
+        pObj.appendChild(spanObj);
+      }else if(i == 1){
+        pObj.className="appendix";
+        let spanObj= document.createElement("span");
+        spanObj.appendChild(data);
+        pObj.appendChild(spanObj);
+      }else if(i == 2) {
+        pObj.className="tweet-content";
+        pObj.appendChild(data);
+      }
+    }
+
+  };
+  conn.onclose = function(event) {
+          alert("切断しました");
+          setTimeout(open, 5000);
+      };
 }
 
-//   conn.onmessage = function(e) {
-//     const data = JSON.parse(e.data);
-//     const divObj = document.createElement("DIV");
-//     if (data["position"] == "left") {
-//       divObj.className = 'receive-msg-left';
-//     } else {
-//       divObj.className = 'receive-msg-right';
-//     }
-//     const msgSplit = data["msg"].split('\n');
-//     for (let i in msgSplit) {
-//       const msg = document.createTextNode(msgSplit[i]);
-//       const rowObj = document.createElement("DIV");
-//       rowObj.appendChild(msg);
-//       divObj.appendChild(rowObj);
-//     }
-//     var msgLog = document.getElementById("msg_log");
-//     msgLog.appendChild(divObj);
-//     var br = document.createElement("BR");
-//     br.className = 'br';
-//     msgLog.appendChild(br);
-//     msgLog.scrollTop = msgLog.scrollHeight;
-//       };
-//     conn.onclose = function() {
-//       alert("切断しました");
-//       setTimeout(open, 5000);
-//     };
-//   }
-// function send(){
-//   conn.send(document.getElementById("msg").value);
-// }
-// function close(){
-//   conn.close();
-// }
-// open();
+function socketSend() {
+  let dateObj = new Date();
+  const fullYear = dateObj.getFullYear();
+  const month = dateObj.getMonth();
+  const date = dateObj.getDate();
+  const hours = dateObj.getHours();
+  const minute = dateObj.getMinutes();
+  const seconds = dateObj.getSeconds();
+  const username = getCookieUsername();
+  const localDate = [fullYear, month, date].join("-");
+  const localTime =[hours, minute, seconds].join(":");
+  let postContent = document.getElementById("js-post-content").value;
+  postContent = htmlentities(postContent);
+  conn.send(username +
+  ">"+postContent+
+  ">"+localDate +" "+ localTime);
+  console.log('send');
+}
 
+function getCookieUsername(){
+    var arr = new Array();
+    if(document.cookie != ''){
+        var tmp = document.cookie.split('; ');
+        for(var i=0;i<tmp.length;i++){
+            var data = tmp[i].split('=');
+            arr[data[0]] = decodeURIComponent(data[1]);
+        }
+    }
+    return arr['username'];
+}
 
-// conn.send('hello world');
+function htmlentities(str){
+  return String(str).replace(/&/g,"&amp;")
+    .replace(/</g,"&lt;")
+    .replace(/>/g,"&gt;")
+    .replace(/"/g,"&quot;")
+}
+
+function close(){
+  conn.close();
+}
+open();
 </script>
 
 <div class='main-all-contents'>
@@ -110,9 +161,10 @@ function open() {
       <div class="close-btn" id="js-close-btn">
         <i class="fas fa-times"></i>
       </div>
-      <button class="tweet-submit-btn" name="send" form="tweet">ツイートする</button>
+      <button class="tweet-submit-btn" name="send" form="tweet" onclick="socketSend();" type="submit">ツイートする</button>
         <form id="tweet" class="tweet-form" method=POST>
-          <textarea class="tweet-textarea"name="tweet-content" cols="" rows="10" wrap= "soft"required ></textarea>
+          <textarea id="js-post-content"
+          class="tweet-textarea"name="tweet-input" cols="" rows="10" wrap= "soft"required ></textarea>
           <p class="tweet-items">
             <span class="tweet-item"><i class="fas fa-bold"></i></span>
             <span class="tweet-item"><i class="fas fa-italic"></i></span>
@@ -125,17 +177,21 @@ function open() {
     </div>
     <div class="black-background" id="js-black-bg"></div>
   </div>
-  <?php foreach($posts as $post):?>
+  <div id="js-posts" class="post"></div>
+  <?php foreach($db_posts as $post):?>
     <div class="post">
-        <p>
-          <img src="" alt="" width="24" height="24">          <span class="tweet-username">
+        <p class="post-user-detail">
+          <img src="" alt="" class ="user-post-img"width="24" height="24">
+          <span class="tweet-username">
             <?php print(fun_h($post['user_name']))?>
-          </span></p>
-          <p><?php print(fun_h($post['tweet_content']))?></p>
-          <p>
-            <a href=""><?php print(fun_h($post['date_time']))?></a>
-            <a href="delete.php" style="color:F33;">削除</a>
-          </p>
+          </span>
+        </p>
+        <p class="tweet-content">
+          <?php print(fun_h($post['tweet_content']))?></p>
+        <p class="appendix">
+          <span><?php print(fun_h($post['date_time']))?></span>
+          <a href="delete.php" style="color:F33;">削除</a>
+        </p>
 
     </div>
   <?php endforeach;?>
